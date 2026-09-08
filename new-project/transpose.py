223a0fab96112a5e82cef63591e3f6b20baa1fee
asalbaharlouie <asalbaharlouie@gmail.com>
2026-09-08 10:04:43 +0330

    matrix transpose

---
+++ new-project/transpose.py
@@ -0,0 +1,13 @@
+def transpose(matrix):
+    if matrix == []:
+        return []
+    result = []
+    for i in range(len(matrix[0])):#روی ستون راه میره
+        new_rows = []
+        for j in range(len(matrix)): #روی سطر راه میره
+            new_rows.append(matrix[j][i])
+        result.append(new_rows)
+
+    return result
+
+print(transpose([[4, 5, 9], [1, 2, 7]]))
